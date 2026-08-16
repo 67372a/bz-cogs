c0ed8ae02ec1b9f8bcd23dd61e9d20adbe67cda3
67372a <117533205+67372a@users.noreply.github.com>
2026-08-16 13:10:19 -0500

    Revert "shim, httpx-aiohttp[httpx2]"
    This reverts commit 247092a65a19fe0eacb0c93703e0a4ef320d3ba7.

--- aiuser/__init__.py
+++ aiuser/__init__.py
@@ -1,9 +1,3 @@
-import aiohttp
-
-# Compatibility shim for openai 3.x vendored httpx_aiohttp on aiohttp >= 3.9
-if not hasattr(aiohttp, "SocketTimeoutError"):
-    aiohttp.SocketTimeoutError = getattr(aiohttp, "ServerTimeoutError", TimeoutError)
-
 from .core.aiuser import AIUser
 from redbot.core.utils import get_end_user_data_statement
 
